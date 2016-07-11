024c8c5b17f06665a959d9dcaf9bd7fd6d88b71e
liuyaoCS <liuyao@chinaso.com>
2016-07-11 17:21:01 +0800

    配置json及依赖包

--- src/cn/ly/servlet/HelloJsonServlet.java
+++ src/cn/ly/servlet/HelloJsonServlet.java
@@ -2,6 +2,8 @@ package cn.ly.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.sf.json.JSONArray;
 import cn.ly.dao.UserDao;
 
 @WebServlet("/easyServlet")
@@ -21,6 +24,41 @@ public class HelloJsonServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//method1(response);
+		
+		method2(response);  
+	
+	}
+
+
+
+	private void method2(HttpServletResponse response) throws IOException {
+		//使用JSONArray测试  
+        JSONArray jsonArray = new JSONArray();  
+        jsonArray.add("MCA");  
+        jsonArray.add("kevin");  
+        jsonArray.add("15-12-1998");  
+        jsonArray.add(new Double(12.3));  
+        List<String> list = new ArrayList<String>();   
+        list.add("a collection added");  
+        list.add("kevin collection test");  
+        jsonArray.addAll(list);  
+          
+        //页面输出JSONArray的内容  
+        response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=utf-8");
+        PrintWriter out = response.getWriter();  
+        out.print(jsonArray);  
+       
+//        out.println("======================================");  
+//        for(int i=0;i<jsonArray.size();i++){  
+//            out.print(jsonArray.getString(i));  
+//        }
+	}
+
+
+
+	private void method1(HttpServletResponse response) {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json; charset=utf-8");
 		String jsonStr = "{\"name\":\"fly\",\"type\":\"虫子\"}";
