691593f25f1487f6bf1c31c8290ebae547b804bd
liuyaoCS <liuyao@chinaso.com>
2016-07-12 21:54:34 +0800

    ajax获取商品信息案例

--- src/cn/ly/servlet/HelloWorld.java
+++ src/cn/ly/servlet/HelloWorld.java
@@ -1,11 +1,17 @@
 package cn.ly.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+
+import net.sf.json.JSONArray;
+import cn.ly.bean.Product;
 
 public class HelloWorld extends GenericServlet {
 
@@ -13,7 +19,16 @@ public class HelloWorld extends GenericServlet {
 	public void service(ServletRequest req, ServletResponse res)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		res.getOutputStream().write("hello servlet".getBytes());
+		List<Product> products=new ArrayList<Product>();
+		products.add(new Product(1,"tv",2000.0));
+		products.add(new Product(2,"phone",1000.0));
+		
+		String json=JSONArray.fromObject(products).toString();
+		//res.getOutputStream().write("hello servlet".getBytes());
+		PrintWriter pw=res.getWriter();
+		pw.print(json);
+		pw.flush();
+		pw.close();
 	}
 
 }
